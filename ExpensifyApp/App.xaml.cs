d4ee1bf41081455219bcbecfd09287369a380152
mukeshmuthu4 <mukesh@abnconsultancyservice.com>
2025-02-20 19:20:28 +0530

    Initial Commit

---
+++ ExpensifyApp/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using ExpensifyApp.Pages;
+
+namespace ExpensifyApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new LoginPage());
+        }
+
+        //protected override Window CreateWindow(IActivationState? activationState)
+        //{
+        //    return new Window(new AppShell());
+        //}
+    }
+}
---
+++ ExpensifyApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpensifyApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
